2c0f5c0146e96b95d10188f81354e32b90962094
ebharucha <ebharucha@gmail.com>
2020-04-16 14:52:08 +0800

    '...'

--- cot.py
+++ cot.py
@@ -3,6 +3,7 @@
 ###########################
 
 # Import dependencies
+import json
 import requests
 from bs4 import BeautifulSoup
 from io import StringIO 
@@ -11,48 +12,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+# Read configuration file
+with open ('cot_cfg.json') as f:
+    cot_cfg = json.load(f)
+
 # Read "Futures-Only Commitments of Traders Comma Delimited" into DataFrame
-URL = 'https://www.cftc.gov/dea/newcot/deafut.txt'
-page = requests.get(URL)
+page = requests.get(cot_cfg['cot_data_url'])
 soup = BeautifulSoup(page.content, 'html.parser')
-soup_strio = StringIO(soup.prettify().replace('&amp;', '&')) # Create an in memory file object, required for pd.read_csv
+soup_strio = StringIO(soup.prettify().replace('&amp;', '&'))
 df_cot = pd.read_csv(soup_strio, header=None)
 
 # Assign column headers 
-with open ('data/cot_data_column_names.txt', 'r') as f:
-    cot_column_names = f.readlines()
-cot_column_names = [line.rstrip() for line in cot_column_names]
-df_cot.columns = cot_column_names
+df_cot.columns = cot_cfg['column_names']
 
 # Choose column headers of interest, to generate short form COT dataset
-with open ('data/cot_columns_select.txt', 'r') as f:
-    cot_columns_select = f.readlines()
-cot_columns_select = [line.rstrip() for line in cot_columns_select]
-df_cot_short = df_cot[cot_columns_select]
-print (df_cot_short.shape)
+df_cot_short = df_cot[cot_cfg['select_column_names']]
 
 # Filter COT data for specific markets & save to a file
-markets = {'E-MINI S&P 500 STOCK INDEX - CHICAGO MERCANTILE EXCHANGE' : 'E-MINI', 
-           'E-MINI RUSSELL 2000 INDEX - CHICAGO MERCANTILE EXCHANGE' : 'RUSSELL',
-           'NASDAQ-100 Consolidated - CHICAGO MERCANTILE EXCHANGE' : 'NASDAQ',
-           'U.S. DOLLAR INDEX - ICE FUTURES U.S.' : '$ Index',
-           'EURO FX - CHICAGO MERCANTILE EXCHANGE' : 'EUR',
-           'BRITISH POUND STERLING - CHICAGO MERCANTILE EXCHANGE' : 'GBP',
-           'JAPANESE YEN - CHICAGO MERCANTILE EXCHANGE' : 'JPY',
-          'CANADIAN DOLLAR - CHICAGO MERCANTILE EXCHANGE' : 'CAD',
-           'AUSTRALIAN DOLLAR - CHICAGO MERCANTILE EXCHANGE' : 'AUD',
-           'NEW ZEALAND DOLLAR - CHICAGO MERCANTILE EXCHANGE' : 'NZD',
-           'BITCOIN - CHICAGO MERCANTILE EXCHANGE' : 'BTC'}
-
 markets_index = [df_cot_short[df_cot_short['Market and Exchange Names'].\
-    str.contains(market)].index.tolist()[0] for market in markets.keys()]
+    str.contains(market)].index.tolist()[0] for market in cot_cfg['markets'].keys()]
 df_cot_short_sel_mkts = df_cot_short.loc[markets_index]
+
 # Create another column ("Markets") with abbreviated names
-f = lambda x: markets[x]
+f = lambda x: cot_cfg['markets'][x]
 df_cot_short_sel_mkts['Markets'] = df_cot_short_sel_mkts['Market and Exchange Names'].apply(f)
+
 # Convert relevant columsn to numeric (avoid any numbers being cast as type string)
 for col in df_cot_short_sel_mkts.columns[2:-1]:
     df_cot_short_sel_mkts[col] = pd.to_numeric(df_cot_short_sel_mkts[col])
+
 # Write short form COT data for selected markets to CSV file
 df_cot_short_sel_mkts.to_csv(f'data/cot_short/cot_short_sel_mkts_{df_cot_short_sel_mkts.iloc[0,1]}.csv')
 
